37091c5296284b196e98c8a1f1bd98469d17a1e3
kimozade <kimozade@gmail.com>
2025-03-03 10:24:09 +0100

    Update Rohlik Study Case.spec.js

--- Rohlik Study Case.spec.js
+++ Rohlik Study Case.spec.js
@@ -1,17 +1,24 @@
 import { test, expect } from '@playwright/test';
 
 test('test', async ({ page }) => {
-    test.setTimeout(120000);
     await page.goto('https://www.rohlik.cz/');
-    // Login
-    await page.locator('[data-test="IconUserLogin"]').click();
+    await page.waitForTimeout(2000);
+    await page.getByRole('button', { name: 'Přijmout vše' }).click();
+    await page.locator('[data-test="header-user-icon"]').click();
     await page.locator('[data-test="user-login-form-email"] [data-test="input"]').click();
     await page.locator('[data-test="user-login-form-email"] [data-test="input"]').fill('kimozade@gmail.com');
     await page.locator('[data-test="user-login-form-password"] [data-test="input"]').click();
     await page.locator('[data-test="user-login-form-password"] [data-test="input"]').fill('1Gfhjkbot!');
     await page.locator('[data-test="btnSignIn"]').click();
-    await page.locator('[data-gtm-section="user-login"]').click();
-    await page.locator('[data-test="my-account-button"]').click();
-    // Function to get the current cart total
-
+    await page.locator('[data-test="productCard-AVAILABLE-1302811"] [data-test="productCard-header-counterButton-plus-button"]').click();
+    await page.locator('[data-test="productCard-header-counterButton-quantity-input"]').click();
+    await page.locator('[data-test="productCard-header-counterButton-quantity-input"]').fill('02');
+    await page.locator('[data-test="productCard-AVAILABLE-1302811"] [data-test="productCard-header-counterButton-plus-button"]').click();
+    await page.locator('[data-test="cart-header-wrapper"]').click();
+    await page.locator('[data-test="cart-redirectToCart"]').click();
+    await page.locator('[data-test="cart-review-button"]').click();
+    await page.locator('[data-test="checkoutForm-select-timeslot"]').click();
+    await page.getByText('Zítra').click();
+    await page.getByRole('button', { name: 'Mezi 12:00–13:00 0,00 Kč' }).click();
+    await expect(page.getByRole('rowgroup')).toContainText('Zdarma');
 });
